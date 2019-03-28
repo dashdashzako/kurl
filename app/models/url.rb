b84960283ad2dad6493fb6ab5b8277b956c58511
dashdashzako <d@dashdashzako.net>
2019-03-28 08:26:54 +0100

    Improve Url validations and tests

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,5 @@
 class Url < ApplicationRecord
-  validates :original, :short, :expires_at, presence: true
+  validates :original, format: { with: /\Ahttps?:\/\/[a-z0-9]{2,}\.[a-z]{2,}.*\z/, message: "given url isn't well formatted" }
+  validates :short, length: { is: 6 }
+  validates :expires_at, presence: true
 end
--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -9,6 +9,34 @@ class UrlTest < ActiveSupport::TestCase
     assert @url.valid?
   end
 
+  test 'invalid when original format is not recognized' do
+    @url.original = nil
+    refute @url.valid?
+    @url.original = ''
+    refute @url.valid?
+    @url.original = 'highflyers.agency'
+    refute @url.valid?
+    @url.original = 'http://.agency'
+    refute @url.valid?
+    @url.original = 'http://highflyers'
+    refute @url.valid?
+    @url.original = 'ftp://highflyers.agency'
+    refute @url.valid?
+    @url.original = 'highflyers.agency'
+    refute @url.valid?
+  end
+
+  test 'valid when original format is recognized' do
+    @url.original = 'http://highflyers.agency/'
+    assert @url.valid?
+    @url.original = 'http://highflyers.agency'
+    assert @url.valid?
+    @url.original = 'https://highflyers.agency'
+    assert @url.valid?
+    @url.original = 'https://www.highflyers.agency/post/47'
+    assert @url.valid?
+  end
+
   test 'invalid without expiration date' do
     @url.expires_at = nil
     refute @url.valid?
