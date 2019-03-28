317b95ad74f957d6fffd1613800ec6085159dd85
dashdashzako <d@dashdashzako.net>
2019-03-28 16:04:15 +0100

    Hide columns from User JSON representation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
   validates_length_of :password, in: 6..20, on: :create
+
+  def as_json(options = {})
+    super(options.merge({ except: %i[password_digest updated_at] }))
+  end
 end
