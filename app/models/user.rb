72bd00160c3837b085359cb51e20306a9228ac46
dashdashzako <d@dashdashzako.net>
2019-03-28 18:00:36 +0100

    Add relationship to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :urls, dependent: :destroy
+
   validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
   validates_length_of :password, in: 6..20, on: :create
 
