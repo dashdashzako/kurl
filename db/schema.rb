d50b44c35879e31ed20b69faf41a73f572b72004
dashdashzako <d@dashdashzako.net>
2019-03-28 15:44:20 +0100

    Prepare db migration for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_171000) do
+ActiveRecord::Schema.define(version: 2019_03_28_144217) do
 
   create_table "urls", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2019_03_26_171000) do
     t.string "short"
     t.datetime "expires_at"
     t.index ["expires_at"], name: "index_urls_on_expires_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
